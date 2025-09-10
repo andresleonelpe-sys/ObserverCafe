14c4477e5caed480b533333bae75ce81ffd7684a
andresleonelpe-sys <andresleonelpe@gmail.com>
2025-09-10 18:45:37 -0500

    Update LoteCafe.java

--- java/model/LoteCafe.java
+++ java/model/LoteCafe.java
@@ -1,13 +1,32 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+ 
 package model;
 
-/**
- *
- * @author andre
- */
 public class LoteCafe {
+    private String varietal;
+    private String notasCata;
+    private String origen;
+    private double altura;
+    private String productor;
+    private double cantidadLibras;
+
     
+public LoteCafe(String varietal, String notasCata, String origen,
+                    double altura, String productor, double cantidadLibras) {
+        this.varietal = varietal;
+        this.notasCata = notasCata;
+        this.origen = origen;
+        this.altura = altura;
+        this.productor = productor;
+        this.cantidadLibras = cantidadLibras;
+    }
+
+    public String getVarietal() { return varietal; }
+    public String getNotasCata() { return notasCata; }
+    public String getOrigen() { return origen; }
+    public double getAltura() { return altura; }
+    public String getProductor() { return productor; }
+    public double getCantidadLibras() { return cantidadLibras; }
 }
+
