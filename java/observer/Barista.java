e5fbeb625a6610f46fcd141dfc22052b7ee4bf96
andresleonelpe-sys <andresleonelpe@gmail.com>
2025-09-10 21:52:11 -0500

    Update Barista.java

--- java/observer/Barista.java
+++ java/observer/Barista.java
@@ -8,6 +8,26 @@ package observer;
  *
  * @author andre
  */
-public class Barista {
-    
-}
+import cafeobserver.model.LoteCafe;
+
+public class Barista implements Observer {
+    private String nombre;
+    private String id;
+
+    public Barista(String nombre, String id) {
+        this.nombre = nombre;
+        this.id = id;
+    }
+
+    @Override
+    public void update(LoteCafe lote) {
+        System.out.println("Notificación para " + nombre + " (ID: " + id + ")");
+        System.out.println("Nuevo lote disponible:");
+        System.out.println("Varietal: " + lote.getVarietal());
+        System.out.println("Notas de cata: " + lote.getNotasCata());
+        System.out.println("Origen: " + lote.getOrigen());
+        System.out.println("Altura: " + lote.getAltura() + " msnm");
+        System.out.println("Productor: " + lote.getProductor());
+        System.out.println("Cantidad: " + lote.getCantidadLibras() + " lbs");
+        System.out.println("--------------------------------------------");
+    }
